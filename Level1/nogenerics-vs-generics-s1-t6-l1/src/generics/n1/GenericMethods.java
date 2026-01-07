581525d59c2972014f58879d34fca85e00db10e3
fonca <eduard.cantos@gmail.com>
2026-01-07 23:37:07 +0100

    feat(session): final version with exercise 2 implementation started

--- Level1/nogenerics-vs-generics-s1-t6-l1/src/generics/n1/GenericMethods.java
+++ Level1/nogenerics-vs-generics-s1-t6-l1/src/generics/n1/GenericMethods.java
@@ -1,4 +1,11 @@
 package generics.n1;
 
-public class GenericMethods {
+class GenericMethods<T> {
+
+    public <T, U, V> void printElements (T element1, U element2, V element3) {
+        System.out.println("Element 1: " + element1);
+        System.out.println("Element 2: " + element2);
+        System.out.println("Element 3: " + element3);
+    }
+
 }
